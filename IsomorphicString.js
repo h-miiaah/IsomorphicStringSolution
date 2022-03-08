f0b07129db38be7e18c74b6f2ac4737006b06341
h-miiaah <haim.miiaah5@gmail.com>
2022-03-07 20:50:31 -0500

    comments

--- IsomorphicString.js
+++ IsomorphicString.js
@@ -36,6 +36,7 @@
   - Create two maps, one for each string.
   - Iterate through each character in the first string, and check if the character is in the map.
   - If it is, we check if the value of the character in the map is the same as the character in the second string.
+  - If it is, we continue to the next character.
   
 
   */
